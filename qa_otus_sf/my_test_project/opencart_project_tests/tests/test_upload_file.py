7ac803c81e08ddbca3d9a9d8f7a8b8cf6ec82d62
Sergey <vumnyi@rambler.ru>
2019-09-06 02:24:58 +0300

    add test upload file with execute js

---
+++ qa_otus_sf/my_test_project/opencart_project_tests/tests/test_upload_file.py
@@ -0,0 +1,40 @@
+import os
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from locators import AdminPage, ProductPage
+import time
+
+
+# рипт для создания формы для загрузки файла
+# browser.execute_script("""
+#     var f = document.createElement("form");
+#     f.id = "form-upload";
+#     f.style.display = "block";
+#     f.enctype = "multipart/form-data";
+#     inp = document.createElement("input");
+#     inp.type = "file";
+#     inp.name = "file";
+#     f.appendChild(inp);
+#     body = document.getElementsByTagName("body")[0];
+#     body.insertBefore(f, body.firstChild);
+# """)
+
+
+def test_upload_file(browser, admin_autorization):
+    """Загружаем файл"""
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'test_img.png')
+    browser.find_element_by_xpath(AdminPage.AdminNavigation.sections('Catalog')).click()
+    WebDriverWait(browser, 5).until(
+        EC.element_to_be_clickable((By.XPATH, AdminPage.AdminNavigation.sections_item('Downloads'))))
+    browser.find_element_by_xpath(AdminPage.AdminNavigation.sections_item('Downloads')).click()
+    browser.find_element_by_xpath(AdminPage.AdminButtonsEditItem.add_new).click()
+    #выполняем клик через js чтобы в DOM появилась форма для загрузки
+    browser.execute_script("document.getElementById('button-upload').click()")
+    browser.find_element_by_xpath("//input[@type='file']").send_keys(filename)
+    WebDriverWait(browser, 5).until(EC.alert_is_present())
+    alert = browser.switch_to.alert
+    alert_text = alert.text
+    assert "Your file was successfully uploaded!" in alert_text
+    alert.accept()
