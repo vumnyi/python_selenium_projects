166273650d20404edf6b6f565705f99c146d7bd5
Sergey Funtikov <35224886+vumnyi@users.noreply.github.com>
2020-07-22 14:27:22 +0300

    Update test_product_page.py

--- page_objects_pattern_some_examples/test_product_page.py
+++ page_objects_pattern_some_examples/test_product_page.py
@@ -5,7 +5,7 @@ from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
 import pytest
 
-
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -56,7 +56,8 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-
+    
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -65,6 +66,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.assert_url_contains('login')
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
@@ -91,6 +93,8 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.should_not_be_success_message()
 
+    
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
         page = ProductPage(browser, link)
